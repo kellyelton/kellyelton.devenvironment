0b871559b02e90201540c08a21484db021933927
Kelly Elton <kelly.elton@skylabsonline.com>
2013-02-09 13:16:55 -0800

    Fixed it

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,25 +9,29 @@ function installsilentlymsi($url, $name) {
   Install-ChocolateyPackage $name 'exe' '/quite' $url
 }
 function installclover(){
-	Install-ChocolateyInstallPackage 'clover' 'exe' '/SILENT' 'CloverSetup.exe'
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Clover\clover.exe"
+	
 }
 try {
-	#cinst VisualStudio2012Professional
-	#Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
-	#cinst msysgit
-	#cinst TortoiseGit
-	#cinst poshgit
-	#cinst tortoisesvn
-	#cinst winscp
-	#cinst nsis
-	#cinst nuget
-	#cinst dragondisk
-	#cinst 7zip.install
-	#Install-ChocolateyPinnedTaskBarItem "${env:SystemRoot}\system32\WindowsPowerShell\v1.0\powershell.exe"
-	#cinst GoogleChrome
-	#Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
-	installclover
+	cinst VisualStudio2012Professional
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
+	cinst msysgit
+	cinst TortoiseGit
+	cinst poshgit
+	cinst tortoisesvn
+	cinst winscp
+	cinst nsis
+	cinst nuget
+	cinst dragondisk
+	cinst 7zip.install
+	Install-ChocolateyPinnedTaskBarItem "${env:SystemRoot}\system32\WindowsPowerShell\v1.0\powershell.exe"
+	cinst GoogleChrome
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+	##Install Clover - Cannot be called within a function I guess
+	$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+	$path = Join-Path $scriptDir "CloverSetup.exe"
+	Install-ChocolateyInstallPackage 'clover' 'exe' '/S' $path
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Clover\clover.exe"
+	
     Write-ChocolateySuccess $packageName
 } catch {
   #Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
