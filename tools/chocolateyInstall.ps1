8715eb6e43e444a0186e6598a701ad38cc244992
Kelly Elton <kellye.elton@skylabsonline.com>
2013-10-28 16:58:20 -0500

    Converted to using dependencies instead of cinst. Replaced VisualStudio2012Pro -> VisualStudio2013Pro Nuget -> Nuget.CommandLine Added VirtualCloneDrive Notepad++ Linqpad4 Putty.Install Vim Paint.Net LastPass

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -12,27 +12,21 @@ function installclover(){
 	
 }
 try {
-	cinst VisualStudio2012Professional
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe"
-	cinst msysgit
-	cinst TortoiseGit
-	cinst poshgit
-	cinst tortoisesvn
-	cinst winscp
-	cinst nsis
-	cinst nuget
-	cinst dragondisk
-	cinst 7zip.install
-	Install-ChocolateyPinnedTaskBarItem "${env:SystemRoot}\system32\WindowsPowerShell\v1.0\powershell.exe"
-	cinst GoogleChrome
-	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
 	##Install Clover - Cannot be called within a function I guess
 	$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 	$path = Join-Path $scriptDir "CloverSetup.exe"
 	Install-ChocolateyInstallPackage 'clover' 'exe' '/S' $path
 	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Clover\clover.exe"
-	cinst virtuawin
-	cinst mysql.workbench
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:SystemRoot}\system32\WindowsPowerShell\v1.0\powershell.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:windir}\system32\mstsc.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\LINQPad4\LINQPad.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\vim\vim73\gvim.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\ChocolateyGUI\ChocolateyGUI.exe"
+	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\WinSCP\WinSCP.exe"
+	ChocolateyPinnedTaskBarItem "%comspec% /k ""${env:ProgramFiles(x86)}\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"""
     Write-ChocolateySuccess $packageName
 } catch {
   #Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
