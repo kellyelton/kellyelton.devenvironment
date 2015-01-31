7c2e29c06270f5cbdb4e3e0f98d545c957e2ca72
Kelly Elton <kelly.elton@skylabsonline.com>
2015-01-31 15:04:51 -0600

    Added IIS, fiddler and snoop

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -21,6 +21,33 @@ try {
 	Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
 	#Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\vim\vim74\gvim.exe"
 	#Install-ChocolateyPinnedTaskBarItem "C:\Chocolatey\lib\winscp.5.1.7\tools\WinSCP.exe"
+    choco windowsfeatures IIS-WebServer
+    choco windowsfeatures IIS-WebServerRole
+    choco windowsfeatures IIS-CommonHttpFeatures
+    choco windowsfeatures IIS-HttpErrors
+    choco windowsfeatures IIS-HttpRedirect
+    choco windowsfeatures IIS-ApplicationDeployment
+    choco windowsfeatures IIS-NetFxExtensibility
+    choco windowsfeatures IIS-NetFxExtensibility45
+    choco windowsfeatures IIS-HealthAndDiagnostics
+    choco windowsfeatures IIS-Security
+    choco windowsfeatures IIS-RequestFiltering
+    choco windowsfeatures IIS-Performance
+    choco windowsfeatures IIS-WebServerManagementTools
+    choco windowsfeatures IIS-ManagementScriptingTools
+    choco windowsfeatures IIS-WindowsAuthentication
+    choco windowsfeatures IIS-StaticContent
+    choco windowsfeatures IIS-DefaultDocument
+    choco windowsfeatures IIS-ApplicationInit
+    choco windowsfeatures IIS-WebSockets
+    choco windowsfeatures IIS-ASPNET
+    choco windowsfeatures IIS-ASPNET45
+    choco windowsfeatures IIS-ASP
+    choco windowsfeatures IIS-CGI
+    choco windowsfeatures IIS-ISAPIExtensions
+    choco windowsfeatures IIS-ISAPIFilter
+    choco windowsfeatures IIS-ManagementConsole
+    choco windowsfeatures IIS-ManagementService
     Write-ChocolateySuccess $packageName
 } catch {
   #Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
